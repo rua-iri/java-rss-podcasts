fcb24893427391b27369f0abba79214cfcd8d6e0
rua-iri <rory.mcguigan@protonmail.com>
2022-11-19 21:56:27 +0000

    basic input form created

--- AppFrame.java
+++ AppFrame.java
@@ -3,31 +3,54 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+
 
 public class AppFrame extends JFrame implements ActionListener{
 
+    JButton button;
+    JTextField textField;
+    JLabel label;
 
     AppFrame() {
 
-        JTextField textField = new JTextField();
-
-
-
-        //set some default styling and behaviour for the window
+        //set up default functioning for window
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+
+        //instantiate and set size of text input
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250, 40));
+
+        //instantiate and establish functioning of button
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        //instantiate label
+        label = new JLabel("RSS URL");
+
+
+        //add all elements and pack
+        this.add(label);
+        this.add(textField);
+        this.add(button);
+        this.pack();
 
         this.setVisible(true);
-
-        this.add(textField);
 
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
+        if(e.getSource()==button) {
+            System.out.println(textField.getText());
+        }
+
+
     }
 
 
